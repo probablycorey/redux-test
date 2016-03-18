923acce02df047ee997b74ec4755f909cb234dcd
probablycorey <probablycorey@github.com>
2016-03-18 16:21:03 -0700

    Use ES6 class

--- containers/AddTodo.js
+++ containers/AddTodo.js
@@ -2,29 +2,37 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
 
-let AddTodo = ({ dispatch }) => {
-  let input
+class AddTodo extends React.Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <div>
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addTodo(input.value))
-        input.value = ''
-      }}>
-        <input ref={node => {
-          input = node
-        }} />
-        <button type="submit">
-          Add Todo
-        </button>
-      </form>
-    </div>
-  )
+    this.props = props
+    this.state = {}
+  }
+
+  onSubmit(event) {
+    event.preventDefault()
+    if (this.input.value.trim() === '') {
+      return
+    }
+    this.props.dispatch(addTodo(this.input.value))
+    this.input.value = ''
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={event => this.onSubmit(event)}>
+          <input ref={node => this.input = node} />
+          <button type='submit'>
+            Add Todo
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
+
 AddTodo = connect()(AddTodo)
 
 export default AddTodo
