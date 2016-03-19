64d334c8cd05c91496cb35179d5aad2e89ccb07e
probablycorey <probablycorey@github.com>
2016-03-19 11:39:28 -0700

    Use ES6 class

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,17 +1,23 @@
 import React, { PropTypes } from 'react'
 
+class Todo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.onClick = props.onClick
+    this.completed = props.completed
+    this.text = props.text
+  }
 
-
-const Todo = ({ onClick, completed, text }) => (
-  <li
-    onClick={onClick}
-    style={{
-      textDecoration: completed ? 'line-through' : 'none'
-    }}
-  >
-    {text}
-  </li>
-)
+  render() {
+    const onClick = event => this.onClick(event)
+    const style = { textDecoration: this.completed ? 'line-through' : 'none' }
+    return (
+      <li onClick={onClick} style={style}>
+        {this.text}
+      </li>
+    )
+  }
+}
 
 Todo.propTypes = {
   onClick: PropTypes.func.isRequired,
